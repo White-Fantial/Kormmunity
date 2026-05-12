038ca1933f35b2fd2eea7321e8cd262bc2a1abdf
Dongho Park <nomadongho@gmail.com>
2026-05-13 10:42:59 +1200

    Adjust header Kormmunity logo size to match title text

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,7 +82,7 @@ export default async function RootLayout({
                   alt="Kormmunity logo"
                   width={210}
                   height={149}
-                  className="h-14 w-auto"
+                  className="h-20 w-auto"
                 />
                 <span>한인 커뮤니티</span>
               </Link>
